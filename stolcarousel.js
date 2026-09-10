df277fb9dc1e86addb10c0d2cf0c0da4962e9a1a
lucinasid14 <lucinasid14@gmail.com>
2026-09-10 09:48:34 +0200

    carousel fix

--- stolcarousel.js
+++ stolcarousel.js
@@ -212,3 +212,40 @@ if ( //control structures. Der skal træffes en beslutning.
     //Hvis vi er gået forbi sidste billede, så gå til billede 0
     //gør carousselen cirkulær, så javascripten ikke bare fortsætter
 }
+
+const carouselItems = document.querySelectorAll(".inspo-card");
+let currentIndex = 0;
+const prevInspoButton = document.querySelector(".inspoCarouselPrev");
+const nextInspoButton = document.querySelector(".inspoCarouselNext");
+
+function showSlide(index) {
+    for(let i = 0; i < carouselItems.length; i++) {
+        carouselItems[i].classList.remove('active');
+    }
+    // problematic for some reason...
+    carouselItems[index].classList.add('active');
+    carouselItems[index + 1].classList.add('active');
+    carouselItems[index + 2].classList.add('active');
+    carouselItems[index + 3].classList.add('active');
+}
+showSlide(currentIndex);
+
+console.log(prevInspoButton);
+
+prevInspoButton.addEventListener('click', function() {
+    currentIndex -= 4;
+    if(currentIndex<0) {
+        // original:         currentIndex = carouselItems.length - 1;
+        currentIndex = 0;
+    }
+    showSlide(currentIndex);
+})
+
+nextInspoButton.addEventListener('click',function() {
+    currentIndex += 4;
+    if(currentIndex > carouselItems.length - 1) {
+        // original:         currentIndex = 0;
+        currentIndex = carouselItems.length - 4;
+    }
+    showSlide(currentIndex);
+})
